4993937f81a3892534983d7ca05839db69d8f204
Meen Tycoon <meenphilip@gmail.com>
2022-06-19 21:39:56 +0300

    init form elements in app.js file

---
+++ app.js
@@ -0,0 +1,5 @@
+// SELECT FORM ELEMENT FROM DOM
+const form = document.getElementById("loan-form");
+const loanAmount = document.querySelector("#loan-amount");
+const AnnualInterestAmount = document.querySelector("#interest-amount");
+const totalAmount = document.querySelector("#total-amount");
