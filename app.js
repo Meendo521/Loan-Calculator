e58094bf47abbb3fdb2aef4d21e558c12140db5b
Meen Tycoon <meenphilip@gmail.com>
2022-06-20 09:48:12 +0300

    added three more variables from UI

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // SELECT FORM ELEMENT FROM DOM
 const form = document.getElementById("loan-form");
 const loanAmount = document.querySelector("#loan-amount");
-const AnnualInterestAmount = document.querySelector("#interest-amount");
-const totalAmount = document.querySelector("#total-amount");
+const interestAmount = document.querySelector("#interest-amount");
+const Years = document.querySelector("#years");
+const monthlyPayment = document.querySelector("#monthly-payment");
+const totalPayment = document.querySelector("#total-payment");
+const totalInterest = document.querySelector("#total-interest");
